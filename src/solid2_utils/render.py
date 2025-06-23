db614ac385338692a33f4d350941d4a95b156f1d
Gerard Choinka <gerard.choinka+github@gmail.com>
2025-06-23 21:54:09 +0100

    refactor render

--- src/solid2_utils/render.py
+++ src/solid2_utils/render.py
@@ -1,22 +1,20 @@
 import argparse
 import logging
+import multiprocessing
 import os
 import re
 import shutil
 import subprocess
-import multiprocessing
 import sys
 import time
 import zipfile
 from dataclasses import dataclass
 from itertools import chain, product
 from pathlib import Path
-from typing import Tuple, Iterable, List, Optional
+from typing import Tuple, Iterable, List
 
-from solid2 import P3, scad_inline
+from solid2 import P3
 from solid2.core.object_base import OpenSCADObject
-
-from solid2_utils.mod import XYZ
 
 
 @dataclass
@@ -29,40 +27,27 @@ class RenderTask:
 @dataclass
 class _RenderTaskArgs:
     scad_object: OpenSCADObject
-    position: XYZ
     filename: Path
     openscad_bin: Path | None
-    file_header: str | None
     verbose: bool
 
-    def __init__(self, task: RenderTask, openscad_bin: Path | None, file_header: str | None, verbose: bool) -> None:
-        self.file_header = file_header
+    def __init__(self, task: RenderTask, openscad_bin: Path | None, verbose: bool) -> None:
         self.scad_object = task.scad_object
-        self.position = task.position
         self.filename = task.filename
         self.openscad_bin = openscad_bin
         self.verbose = verbose
 
 
-def _line_prepender(filename, line):
-    with open(filename, 'r+') as f:
-        content = f.read()
-        f.seek(0, 0)
-        f.write(line.rstrip('\r\n') + '\n' + content)
-
-
 def _render_to_file(task: _RenderTaskArgs) -> Tuple[Path, float]:
     scad_filename = task.filename.with_suffix(".scad").absolute().as_posix()
     task.scad_object.save_as_scad(scad_filename)
-    _line_prepender(scad_filename, task.file_header)
     elapsed = 0.0
     manifold = True
     extra_cli_args = ["--backend", "Manifold"] if manifold else []
     if task.openscad_bin is not None:
         out_filenames = tuple(chain.from_iterable(
             product(("-o",), (task.filename.with_suffix(ext).absolute().as_posix() for ext in (".3mf", ".png")))))
-        openscad_cli_args = [task.openscad_bin, *out_filenames, *extra_cli_args,
-                             "--colorscheme", "BeforeDawn",
+        openscad_cli_args = [task.openscad_bin, *out_filenames, *extra_cli_args, "--colorscheme", "BeforeDawn",
                              scad_filename]
         return_code = None
         try:
@@ -108,9 +93,9 @@ def set_model_name(filename: Path, name: str) -> None:
     shutil.move(new_filename, old_filename)
 
 
-def save_to_file(output_scad_basename: Path, openscad_bin: Path | None, render_tasks: Iterable[RenderTask],
-                 all_filename: Path = Path("all"), include_filter_regex: re.Pattern[str] | None = None,
-                 remove_duplicates=True, verbose: bool = False, _fn: int = 180) -> None:
+def save_to_file(openscad_bin: Path | None, render_tasks: Iterable[RenderTask],
+                 include_filter_regex: re.Pattern[str] | None = None, remove_duplicates=True,
+                 verbose: bool = False) -> None:
     if verbose:
         from multiprocessing.dummy import Pool
     else:
@@ -122,27 +107,11 @@ def save_to_file(output_scad_basename: Path, openscad_bin: Path | None, render_t
                             zip((r.filename for r in render_tasks), range(len(render_tasks_list)))}
         render_tasks_list = [render_tasks_list[idx] for idx in unique_scads_idx.values()]
 
-    file_header = f"$fn = {_fn};\n"
-    render_tasks_args: List[_RenderTaskArgs] = [_RenderTaskArgs(t, openscad_bin, file_header, verbose) for t in
-                                                render_tasks_list]
+    render_tasks_args: List[_RenderTaskArgs] = [_RenderTaskArgs(t, openscad_bin, verbose) for t in render_tasks_list]
     if include_filter_regex is not None:
         render_tasks_args = [t for t in render_tasks_args if include_filter_regex.search(t.filename.as_posix())]
 
     logging.info(f"Will generate {", ".join(task.filename.as_posix() for task in render_tasks_args)}", )
-
-    all_obj: Optional[OpenSCADObject] = None
-    for task_args in render_tasks_args:
-        task_args.filename = output_scad_basename.joinpath(task_args.filename)
-        if verbose:
-            task_args.scad_object += scad_inline(f'echo("Writing {task_args.filename.as_posix()}");\n')
-        if all_obj is None:
-            all_obj = task_args.scad_object.translate(task_args.position)
-        else:
-            all_obj += task_args.scad_object.translate(task_args.position)
-
-    scad_filename = output_scad_basename.joinpath(all_filename).with_suffix(".scad")
-    if all_obj is not None:
-        all_obj.save_as_scad(scad_filename.absolute().as_posix())
 
     with Pool(max(multiprocessing.cpu_count() - 2, 1)) as pool:
         for filename, elapsed in pool.map(_render_to_file, render_tasks_args):
